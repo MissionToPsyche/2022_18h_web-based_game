0be10d0ae810c9aa89a4a1858229569d1ba30036
ethan-williams <ognid765@gmail.com>
2022-10-16 02:26:43 -0700

    :twisted_rightwards_arrows: Merge unit-test-setup with develop to begin adding unit tests

--- test/test.example_function.js
+++ test/test.example_function.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var add = require('../example_function.js').add;
 describe("add", function() {
 
-  it("returns the sum of two inputs", function() {
+  it("should return the sum of two inputs", function() {
     assert.equal(add(2, 3), 5);
     assert.equal(add(-2, 3), 1);
     assert.equal(add(0, 0), 0);
---
+++ test/test.sketch.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+var sketch = require('../sketch.js');
+var draw = sketch.draw;
+
+describe("setUpBodies", function() {
+    //todo
+});
