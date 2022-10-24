adb91a085a86d4e8f324d0ec1adf36e3b1d961a7
Bsneal <BrendanSNeal@gmail.com>
2022-10-23 17:47:52 -0700

    Fixed a few problems that arose when pulling dev to make sure everything merged properly.

--- GravUtils.js
+++ GravUtils.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const G = 6.67
 
 //basic calculation for gravity. b1 and b2 are the two bodies involved.
@@ -12,29 +11,4 @@ function calcGravity (m1, m2, r) {
 function gaussLaw (f, m1) {
     var g = new Phaser.Math.Vector2(f.x/m1, f.y/m1);
 	return g;
-=======
-/*****************************
-GravUtils
-- An object prototype containing various util funcitons for gravity 
-*****************************/
-class GravUtils {
-	constructor() {
-		if (this instanceof GravUtils) {
-			//This is a static object, and the constructor should not be called
-			throw Error('This is a static class')
-		}
-	}
-
-	//basic calculation for gravity. b1 and b2 are the two bodies involved.
-	//R is the radius between the two
-	static calcGravity (m1, m2, r) {
-		// this is Newton's Law of Universal Gravitation (https://en.wikipedia.org/wiki/Newton%27s_law_of_universal_gravitation)
-		return G * (m1 * m2) / (r * r);
-	}
-
-	//calculates the gravitational acceleration on a mass based on force
-	static gaussLaw (f, m1) {
-		return createVector(f.x/m1, f.y/m1)
-	}
->>>>>>> 544aa3acbd1b85496d81ab6aeaf463ebd726090a
 }
