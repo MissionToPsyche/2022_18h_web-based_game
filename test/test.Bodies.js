4869877673c9332fed5a50919f02929993b0a672
ethan-williams <ognid765@gmail.com>
2022-10-30 21:40:00 -0700

    :white_check_mark: Add new tests for Bodies

--- test/test.Bodies.js
+++ test/test.Bodies.js
@@ -1,6 +1,6 @@
 'use-strict';
 
-describe('Bodies tests', function() {
+describe('Body tests', function() {
     let body;
 
     beforeEach(function() {
@@ -15,6 +15,74 @@ describe('Bodies tests', function() {
 
     it('should be an object', function(done) {
         expect(body).to.be.a('object');
+        done();
+    });
+});
+
+describe('Planet tests', function() {
+    let body;
+    let planet;
+
+    beforeEach(function() {
+        var id = 'testBody';
+        var mass = '100';
+        var diameter = '15';
+        var pos = new Phaser.Geom.Point(0, 0);
+        var vel = new Phaser.Math.Vector2(0, 0);
+        var distance = 100;
+
+        body = new Body(id, mass, diameter, pos, vel);
+        planet = new Planet(id, mass, diameter, body, distance, pos, vel);
+    });
+
+    it('should have a distance parameter', function(done) {
+        expect(planet).to.have.property('distance');
+        done();
+    });
+});
+
+describe('Satellite tests', function() {
+    let body;
+    let satellite;
+
+    beforeEach(function() {
+        var id = 'testBody';
+        var mass = '100';
+        var diameter = '15';
+        var pos = new Phaser.Geom.Point(0, 0);
+        var vel = new Phaser.Math.Vector2(0, 0);
+        var distance = 100;
+
+        body = new Body(id, mass, diameter, pos, vel);
+        satellite = new Satellite(id, mass, diameter, body, distance, pos, vel);
+    });
+
+    it('should have a parent parameter', function(done) {
+        expect(satellite).to.have.property('parent');
+        done();
+    });
+
+    it('should successfully parent to another Body', function(done) {
+        expect(satellite.parent).to.be.a('object');
+        done();
+    });
+});
+
+describe('Probe tests', function() {
+    let probe;
+
+    beforeEach(function() {
+        var id = 'testBody';
+        var mass = '100';
+        var diameter = '15';
+        var pos = new Phaser.Geom.Point(0, 0);
+        var vel = new Phaser.Math.Vector2(0, 0);
+
+        probe = new Probe(id, mass, diameter, pos, vel);
+    });
+
+    it('should initialize at the position of the sun', function(done) {
+        //todo
         done();
     });
 });
--- test/test.example_function.js
+++ test/test.example_function.js
@@ -1,10 +1,11 @@
 //var assert = require('assert');
 //var add = require('../example_function.js').add;
-describe("add", function() {
-  it("should return the sum of two inputs", function() {
+describe("Add example tests", function() {
+  it('should return the sum of two inputs', function(done) {
     assert.equal(add(2, 3), 5);
     assert.equal(add(-2, 3), 1);
     assert.equal(add(0, 0), 0);
     assert.equal(add(-12345, -12345), -24690);
+    done();
   });
 });
