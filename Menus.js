9ae3615cc67225cb32139331cde39634fb3a7a6e
Avery camou <ecwilli8@asu.edu>
2023-01-25 15:27:05 -0700

    :construction: Begin adding JSDoc style documentation to Menus.js

--- Menus.js
+++ Menus.js
@@ -1,9 +1,17 @@
+/**
+ * Class representing a menu
+ * A menu is composed of its elements, such as images, text and interactive buttons
+ */
 class Menu {
 	constructor(_scene) {
 		this.scene = _scene;
 		this.elements = [];
 	}
 
+	/**
+	 * Allows you to add any element to this menu, such as an image or text
+	 * @param {object} _element - An element that will belong to this menu
+	 */
 	addElement(_element) {
 		_element.setVisible(false);
 		this.elements.push(_element);
