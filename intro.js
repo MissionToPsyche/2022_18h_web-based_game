30cba0d8448b6133f2a4a66baedad41cd4075b99
Daryl Susilo <slmd06@gmail.com>
2023-01-27 19:44:45 -0800

    Made pages for the introduction scene. Player can click an arrow button and flip trough the introduction to gain better knowledge of the game.

--- intro.js
+++ intro.js
@@ -1,11 +1,15 @@
 class Intro extends Phaser.Scene {
     constructor() {
         super({ key: "Intro" });
+        this.page;
+        this.nextButton;
+        this.introText;
     }
 
     preload() {
         this.load.image('logo', 'img/Psyche_Icon_Color-SVG.svg'); //asset for psyche logo
         this.load.image('border', 'img/icons/intro-border.png');
+        this.page = 1;
     }
 
     create() {
@@ -25,8 +29,49 @@ class Intro extends Phaser.Scene {
         this.shadow.setAlpha(0.5);
         this.shadow.setVisible(true);
         CameraManager.addUISprite(this.shadow);
+        this.createPage();
     }
 
     update() {
+        this.updatePage();
     }
+
+    updatePage() {
+
+        switch(this.page){
+            case 2:
+                this.introText.setVisible(false);
+                this.introText = this.add.text(325, 100, 'Page 2').setOrigin(0.5).setFontSize(50);
+                break;
+            case 3:
+                this.introText.setVisible(false);
+                this.introText = this.add.text(325, 100, 'Page 3').setOrigin(0.5).setFontSize(50);
+                this.nextButton.setVisible(false);
+                break;
+        }
+
+        CameraManager.addUISprite(this.introText);
+    }
+
+    createPage() {
+        this.nextButton = this.add.triangle(700, 650, 0, 128, 64, 0, 128, 128, 0x6666ff).setScale(0.35);
+        this.nextButton.angle = 90
+        this.introText = this.add.text(325, 100, 'Page 1').setOrigin(0.5).setFontSize(50);
+        CameraManager.addUISprite(this.nextButton);
+        CameraManager.addUISprite(this.introText);
+        this.nextButton.setInteractive()
+        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () => {
+            this.nextButton.setFillStyle(0xF9A000);
+        })
+        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, () => {
+            this.nextButton.setFillStyle(0x6666ff);
+        })
+        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            this.nextButton.setFillStyle(0xF47D33);
+        })
+        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, () => {
+            this.page = this.page + 1;
+        });
+    }
+
 }
