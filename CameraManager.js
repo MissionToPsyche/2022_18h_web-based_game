03839f80da45738d01e432b575b618084dec6253
Bsneal <BrendanSNeal@gmail.com>
2023-02-27 08:34:29 -0700

    Some camera changes to how orbit locking works

--- CameraManager.js
+++ CameraManager.js
@@ -84,7 +84,7 @@ class CameraManager {
      */
     static setCameraZoom(zoom) {
         this.mainZoom = zoom;
-        this.mainCamera.zoomTo(zoom);
+        this.mainCamera.zoomTo(zoom, 2000, 'Linear', true);
     }
 
     /**
@@ -97,14 +97,14 @@ class CameraManager {
         //need to find what the fraction of the zoom
         //is based on the camera's viewport size
         var zoom = this.mainCamera.height / size;
-        this.mainCamera.zoomTo(zoom, 1000, 'Linear', true);
+        this.mainCamera.zoomTo(zoom, 2000, 'Quad.easeIn', true);
     }
 
     /**
      * returns the main camera to it's set zoom value.
      */
     static returnToSetZoom() {
-        this.mainCamera.zoomTo(this.mainZoom, 1000, 'Linear', true);
+        this.mainCamera.zoomTo(this.mainZoom, 2000, 'Quad.easeOut', true);
     }
 
     /**
