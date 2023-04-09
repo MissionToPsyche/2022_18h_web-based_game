eaef17528de5a61e19f003e21a41dfc36d94f255
Avery camou <ecwilli8@asu.edu>
2023-04-08 21:33:51 -0700

    :memo: Update example test to work on the backend

--- example_function.js
+++ example_function.js
@@ -1,5 +1,6 @@
 // Example function to unit test, returns the sum of its inputs
-
 function add(num1, num2) {
     return num1 + num2;
 }
+
+module.exports = { add };
--- test/test.example_function.js
+++ test/test.example_function.js
@@ -1,11 +1,12 @@
-//var assert = require('assert');
-//var add = require('../example_function.js').add;
+var assert = require('assert');
+var example = require('../example_function.js');
+
 describe("Add example tests", function() {
   it('should return the sum of two inputs', function(done) {
-    assert.equal(add(2, 3), 5);
-    assert.equal(add(-2, 3), 1);
-    assert.equal(add(0, 0), 0);
-    assert.equal(add(-12345, -12345), -24690);
+    assert.equal(example.add(2, 3), 5);
+    assert.equal(example.add(-2, 3), 1);
+    assert.equal(example.add(0, 0), 0);
+    assert.equal(example.add(-12345, -12345), -24690);
     done();
   });
 });
